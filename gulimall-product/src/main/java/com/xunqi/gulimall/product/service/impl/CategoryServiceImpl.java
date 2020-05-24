411b090734e3d7d3f9efef105354f6e813e85a0e
夏沫止水 <1468041654@qq.com>
2020-05-24 11:48:58 +0800

    商品服务-API-三级分类-删除-逻辑删除

--- gulimall-product/src/main/java/com/xunqi/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/xunqi/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -54,11 +54,19 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return levelMenus;
     }
 
+    @Override
+    public void removeMenuByIds(List<Long> asList) {
+        //TODO 检查当前删除的菜单，是否被别的地方引用
+
+        //逻辑删除
+        baseMapper.deleteBatchIds(asList);
+    }
+
 
     //递归查找所有菜单的子菜单
     private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all) {
 
-        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity ->{
             return categoryEntity.getParentCid().equals(root.getCatId());
         }).map(categoryEntity -> {
             //1、找到子菜单(递归)
