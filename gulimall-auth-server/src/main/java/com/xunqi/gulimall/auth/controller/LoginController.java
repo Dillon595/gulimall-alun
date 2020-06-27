380ce2e5ef87700615e0c9e8801761d10e595a0f
夏沫止水 <1468041654@qq.com>
2020-06-27 10:02:12 +0800

    商城业务-认证服务-整合短信验证码

---
+++ gulimall-auth-server/src/main/java/com/xunqi/gulimall/auth/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.xunqi.gulimall.auth.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description:
+ * @Created: with IntelliJ IDEA.
+ * @author: 夏沫止水
+ * @createTime: 2020-06-24 10:37
+ **/
+
+@Controller
+public class LoginController {
+
+
+
+}
