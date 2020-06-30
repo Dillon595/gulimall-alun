ee60d7367024606043e39256ce77e015464822f0
夏沫止水 <1468041654@qq.com>
2020-06-30 11:19:20 +0800

    商城业务-购物车-环境搭建

---
+++ gulimall-cart/src/main/java/com/xunqi/gulimall/cart/GulimallCartApplication.java
@@ -0,0 +1,16 @@
+package com.xunqi.gulimall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCartApplication.class, args);
+    }
+
+}
