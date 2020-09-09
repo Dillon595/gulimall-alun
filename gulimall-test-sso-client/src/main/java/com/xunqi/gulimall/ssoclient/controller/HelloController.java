af20ab78760d46df5c40197ecd36b1388e0bd93a
974751082@qq.com <974751082@qq.com>
2020-09-10 05:45:14 +0800

    完善sso登录demo

--- gulimall-test-sso-client/src/main/java/com/xunqi/gulimall/ssoclient/controller/HelloController.java
+++ gulimall-test-sso-client/src/main/java/com/xunqi/gulimall/ssoclient/controller/HelloController.java
@@ -1,9 +1,13 @@
 package com.xunqi.gulimall.ssoclient.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -22,6 +26,7 @@ public class HelloController {
 
     /**
      * 无需登录就可访问
+     *
      * @return
      */
     @ResponseBody
@@ -32,19 +37,31 @@ public class HelloController {
 
 
     @GetMapping(value = "/employees")
-    public String employees(Model model, HttpSession session) {
-        List<String> emps = new ArrayList<>();
+    public String employees(Model model, HttpSession session, @RequestParam(value = "token", required = false) String token) {
 
+        if (!StringUtils.isEmpty(token)) {
+            RestTemplate restTemplate=new RestTemplate();
+            ResponseEntity<String> forEntity = restTemplate.getForEntity("http://sso.mroldx.cn:8080/userinfo?token=" + token, String.class);
+            String body = forEntity.getBody();
+
+            session.setAttribute("loginUser", body);
+        }
         Object loginUser = session.getAttribute("loginUser");
 
         if (loginUser == null) {
-            //没登录,跳转到服务器登录
+
+            return "redirect:" + "http://sso.mroldx.cn:8080/login.html"+"?redirect_url=http://localhost:8081/employees";
         } else {
+
+
+            List<String> emps = new ArrayList<>();
+
             emps.add("张三");
             emps.add("李四");
+
+            model.addAttribute("emps", emps);
+            return "employees";
         }
-        model.addAttribute("emps",emps);
-        return "employees";
     }
 
 }
